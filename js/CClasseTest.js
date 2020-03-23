d4461db80f01a05b774de859b06ba7c8f8676d44
JdeDev <julien@jdedev.fr>
2020-03-23 14:26:59 +0100

    modif en gestion tableau

--- js/CClasseTest.js
+++ js/CClasseTest.js
@@ -5,7 +5,9 @@ class voiture {
     couleurText;
     texts;
     vitesse;
-    constructor(image, posX, posY, vitesse) {
+    numVoit;
+    constructor(numVoit, image, posX, posY, vitesse) {
+        this.numVoit = numVoit;
         this.image = image;
         this.posX = posX;
         this.posY = posY;
@@ -28,7 +30,7 @@ class voiture {
         let creation = false;
 
         //Ma voiture existe t'elle ?
-        if (document.getElementById('vroum') == null) {
+        if (document.getElementById("vroum" + this.numVoit) == null) {
             //non, on la créé
             monElem = document.createElement('div');
             creation = true;
@@ -36,10 +38,10 @@ class voiture {
         else {
             //oui, on la récupère
             console.log("Mon elem Existe")
-            monElem = document.getElementById('vroum');
+            monElem = document.getElementById("vroum" + this.numVoit);
         }
         monElem.style.position = "fixed";
-        monElem.id = "vroum"
+        monElem.id = "vroum" + this.numVoit;
         monElem.style.top = this.posY + "px";
         monElem.style.left = this.posX + "px";
         monElem.style.width = "150px";
@@ -48,14 +50,27 @@ class voiture {
         monElem.style.backgroundRepeat = "no-repeat";
         monElem.style.backgroundSize = "contain";
 
-        monElem.addEventListener('click', () => {
-            console.log("on bouge !")
-            onBouge = true;
-        })
+
+
 
         //Si ma voiture n'existe pas je l'ajoute au html
         if (creation) {
             document.body.appendChild(monElem);
+            monElem.innerHTML += "mon Num = " + this.numVoit
+            jdeAttachElem("vroum" + this.numVoit, "div", ["basDroite"], "menBas" + this.numVoit)
+            jdeAttachElem("menBas" + this.numVoit, 'i', ["fas", "fa-arrows-alt"], "", () => {
+                idBouge = this.numVoit;
+                onBouge = true;
+            });
+            jdeAttachElem("menBas" + this.numVoit, 'i', ["fas", "fa-trash-alt"], "", () => {
+                document.getElementById("vroum" + this.numVoit).remove()
+                delVoit(this.numVoit)
+            });
+
+
         }
+
+
+
     }
 }
--- js/vroum.js
+++ js/vroum.js
@@ -1,9 +1,11 @@
 let onBouge = false;
 let x;
 let y;
+let idBouge = 0;
+var tabDingo = new Array();
 
-let maVoit = new voiture("voiture.jpg", 50, 50, 12);
-maVoit.afficheTest();
+
+
 
 document.addEventListener('mousemove', e => {
     x = e.clientX;
@@ -11,16 +13,47 @@ document.addEventListener('mousemove', e => {
 })
 document.addEventListener('mouseup', () => {
     console.log("on stop !")
+    idBouge = 0
     onBouge = false;
 })
 
+document.getElementById('dingPlus').addEventListener('mousedown', () => {
+    //idBouge = tabDingo.length + 1;
+    let voit = new voiture(tabDingo.length + 1, "voiture.jpg", 100, 100, 12)
+    tabDingo.push(voit);
+    //onBouge = true;
+    tabDingo[(tabDingo.length - 1)].afficheTest();
+
+});
 function refresh() {
-    if (onBouge) {
+    if (onBouge && idBouge != 0) {
         console.log("on déplace !")
-        maVoit.changePlace(x, y);
-        maVoit.afficheTest();
+        tabDingo[(idBouge - 1)].changePlace(x, y);
+        tabDingo[(idBouge - 1)].afficheTest();
     }
     setTimeout(refresh, 300)
 }
 
 refresh();
+
+function jdeAttachElem(parentId, elem, classElem = [], idElem = "", fonct = "") {
+    let elemACreer = document.createElement(elem)
+    if (classElem.length > 0) {
+        for (let uneClasse in classElem) {
+            elemACreer.classList.add(classElem[uneClasse])
+        }
+
+    }
+    if (idElem != "") {
+        elemACreer.id = idElem
+    }
+    if (fonct != "") {
+        elemACreer.addEventListener('click', fonct);
+    }
+    document.getElementById(parentId).appendChild(elemACreer)
+
+}
+
+function delVoit(numVoit) {
+    delete tabDingo[numVoit - 1]
+}
